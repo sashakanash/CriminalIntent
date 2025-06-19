bdb8068ebf9db97ed71797adf4ccd28bb50e9c92
Sasha K <sashakanash@bk.ru>
2025-06-19 19:01:50 +0700

    Переопределена onCreateView

--- app/src/main/java/com/sample/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/sample/criminalintent/CrimeFragment.kt
@@ -1,6 +1,24 @@
 package com.sample.criminalintent
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class CrimeFragment : Fragment() {
+
+    private lateinit var crime: Crime
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_crime,container, false)
+        return view
+    }
+
 }
