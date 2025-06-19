3937e1af6b8e4cdecea00b1558660dbfdfc9ebf1
Sasha K <sashakanash@bk.ru>
2025-06-19 19:15:38 +0700

    Настройка виджета EditText, добавление слушателя к виджету EditText

--- app/src/main/java/com/sample/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/sample/criminalintent/CrimeFragment.kt
@@ -1,14 +1,18 @@
 package com.sample.criminalintent
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 
 class CrimeFragment : Fragment() {
 
     private lateinit var crime: Crime
+    private lateinit var titleField: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +22,27 @@ class CrimeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_crime,container, false)
+        titleField = view.findViewById(R.id.crime_title) as EditText
         return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val titleWatcher = object: TextWatcher
+        {
+            override fun beforeTextChanged(sequence: CharSequence?, start: Int, count: Int, after: Int) {
+                // Это пространство оставлено пустым специально
+            }
+
+            override fun onTextChanged(sequence: CharSequence?, start: Int, before: Int, count: Int) {
+                crime.title = sequence.toString()
+            }
+
+            override fun afterTextChanged(sequence: Editable?) {
+                // И это
+            }
+        }
+        titleField.addTextChangedListener(titleWatcher)
     }
 
 }
