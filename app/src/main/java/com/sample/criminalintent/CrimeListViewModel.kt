931f65427dcf389be89d6f43d8926657c9fa6489
Sasha K <sashakanash@bk.ru>
2025-06-19 21:08:45 +0700

    Добавлены классы CrimeHolder и CrimeAdapter

--- app/src/main/java/com/sample/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/sample/criminalintent/CrimeListViewModel.kt
@@ -8,7 +8,7 @@ class CrimeListViewModel: ViewModel() {
     init {
         for (i in 0 until 100) {
             val crime = Crime()
-            crime.title = "Crime #%i"
+            crime.title = "Crime #$i"
             crime.isSolved = i % 2 == 0
             crimes += crime
         }
