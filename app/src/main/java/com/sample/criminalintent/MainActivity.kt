e5ba3163257134ac4e0c9f0d268420d8777d3635
Sasha K <sashakanash@bk.ru>
2025-06-19 19:33:55 +0700

    Создан макета контейнера фрагмента, добавлен CrimeFragment

--- app/src/main/java/com/sample/criminalintent/MainActivity.kt
+++ app/src/main/java/com/sample/criminalintent/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.sample.criminalintent
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null)
+        {
+            val fragment = CrimeFragment()
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
         }
     }
 }
