092e5302785bb2da8061b2bc42b3075ea6e46562
Sasha K <sashakanash@bk.ru>
2025-06-19 19:22:09 +0700

    Настройка текста Button

--- app/src/main/java/com/sample/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/sample/criminalintent/CrimeFragment.kt
@@ -6,6 +6,7 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 
@@ -13,6 +14,7 @@ class CrimeFragment : Fragment() {
 
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
+    private lateinit var dateButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +22,15 @@ class CrimeFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View?
+    {
         val view = inflater.inflate(R.layout.fragment_crime,container, false)
         titleField = view.findViewById(R.id.crime_title) as EditText
+        dateButton =  view.findViewById(R.id.crime_date) as Button
+        dateButton.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
         return view
     }
 
