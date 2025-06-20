e4e678df3254968d5482cb932036a303558da48c
Sasha K <sashakanash@bk.ru>
2025-06-20 16:20:44 +0700

    Создан и подключен подкласс приложения с инициализацией репозитория

---
+++ app/src/main/java/com/sample/criminalintent/CriminalIntentApplication.kt
@@ -0,0 +1,10 @@
+package com.sample.criminalintent
+
+import android.app.Application
+
+class CriminalIntentApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CrimeRepository.initialize(this)
+    }
+}
