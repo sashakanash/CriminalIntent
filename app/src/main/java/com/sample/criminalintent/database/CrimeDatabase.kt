d999710c19a8426189848623aef86b587b249fdf
Sasha K <sashakanash@bk.ru>
2025-06-20 14:12:18 +0700

    Создали базу данных и преобразователь типов

---
+++ app/src/main/java/com/sample/criminalintent/database/CrimeDatabase.kt
@@ -0,0 +1,12 @@
+package com.sample.criminalintent.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.sample.criminalintent.Crime
+
+@Database(entities = [Crime::class],version=1)
+@TypeConverters(CrimeTypeConverters::class)
+abstract class CrimeDatabase : RoomDatabase(){
+
+}
